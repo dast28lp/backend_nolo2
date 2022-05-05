bb2531bc843b5dc21d7d343a6295dbeea8a8c9a7
andres Mora <102624292+andresuniminutov@users.noreply.github.com>
2022-05-04 20:33:34 -0500

    [update] Changed endpoints of rest Added cors validation

--- src/main/java/com/nolo/nolo/ServiceController.java
+++ src/main/java/com/nolo/nolo/ServiceController.java
@@ -1,24 +1,26 @@
 package com.nolo.nolo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class ServiceController {
 
     @Autowired
     private ServiceRepository serviceRepository;
 
-    @GetMapping("/")
+    @GetMapping("/service")
     public @ResponseBody Iterable<Service> getServices() {
         return serviceRepository.findAll();
     }
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/service")
     public @ResponseBody String addNewUser(@RequestBody ServiceDto serviceDto) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
